a6145be114d1e97fcf7e3e114bf60c68ae9d10ca
Steven Yoo <yooste1989@gmail.com>
2023-05-18 01:23:59 -0500

    finishing day one part two

--- src/day_one/calculate_final_floor.rs
+++ src/day_one/calculate_final_floor.rs
@@ -8,9 +8,11 @@ pub fn calculate_final_floor(floors: String) -> i32 {
     } else {
       down += 1;
     }
+
+    if up - down < 0 { break; }
   }
 
-  up - down
+  up + down
 }
 
 #[cfg(test)]
@@ -19,17 +21,14 @@ mod calculate_final_floor_tests {
 
   #[test]
   fn final_floor_test() {
-    let final_floors_equal: [i32; 5] = [
-      calculate_final_floor(String::from("()()")),
-      calculate_final_floor(String::from("(((")),
-      calculate_final_floor(String::from("(()(()(")),
-      calculate_final_floor(String::from(")))")),
-      calculate_final_floor(String::from(")())())")),
+    let final_floors_equal: [i32; 2] = [
+      calculate_final_floor(String::from(")")),
+      calculate_final_floor(String::from("()())")),
     ];
 
     assert_eq!(final_floors_equal, 
-      [0, 3, 3, -3, -3], 
-      "floors did not evaluate to 0"
+      [1, 5], 
+      "did not enter basement at correct position"
     )
   }
 }
